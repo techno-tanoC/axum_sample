c3c32d7f9e4799c569379889bd944be43335e28b
techno-tanoC <develop.tanoc@gmail.com>
2021-07-31 14:43:56 +0900

    Add path_params1

---
+++ examples/path_params.rs
@@ -0,0 +1,21 @@
+use axum::{
+    extract,
+    prelude::*,
+};
+use std::net::SocketAddr;
+
+#[tokio::main]
+async fn main() {
+    let app = route("/users/:user_id/posts/:post_id", get(user_post));
+    let addr = SocketAddr::from(([127, 0, 0, 1], 3000));
+    hyper::Server::bind(&addr)
+        .serve(app.into_make_service())
+        .await
+        .unwrap();
+}
+
+async fn user_post(extract::UrlParams(params): extract::UrlParams<(u64, u64)>) -> String {
+    let user_id = params.0;
+    let post_id = params.1;
+    format!("user_id: {}, post_id: {}", user_id, post_id)
+}
