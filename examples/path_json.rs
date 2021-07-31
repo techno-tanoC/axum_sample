5f15bf6eaa39031ab26d506e0a233aa3a5a526c5
techno-tanoC <develop.tanoc@gmail.com>
2021-07-31 16:46:02 +0900

    Add path_json example

---
+++ examples/path_json.rs
@@ -0,0 +1,38 @@
+use axum::{
+    extract,
+    response,
+    prelude::*,
+};
+use serde::{Serialize, Deserialize};
+use std::net::SocketAddr;
+
+#[tokio::main]
+async fn main() {
+    let app = route("/users/:user_id", post(user_message));
+    let addr = SocketAddr::from(([127, 0, 0, 1], 3000));
+    hyper::Server::bind(&addr)
+        .serve(app.into_make_service())
+        .await
+        .unwrap();
+}
+
+#[derive(Deserialize)]
+struct Name {
+    name: String,
+}
+
+#[derive(Serialize)]
+struct Message {
+    message: String,
+}
+
+async fn user_message(
+    extract::UrlParams(params): extract::UrlParams<(u64,)>,
+    extract::Json(name): extract::Json<Name>,
+) -> response::Json<Message> {
+    let user_id = params.0;
+    let name = name.name;
+    response::Json(Message {
+        message: format!("Hello {}, your id is {}", name, user_id),
+    })
+}
