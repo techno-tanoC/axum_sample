6dac7e98d6e57dff9afadf2b2752a4cea26bbae5
techno-tanoC <develop.tanoc@gmail.com>
2021-07-31 14:11:33 +0900

    json api

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,14 @@
-use axum::prelude::*;
+use axum::{
+    extract,
+    response,
+    prelude::*,
+};
+use serde::{Serialize, Deserialize};
 use std::net::SocketAddr;
 
 #[tokio::main]
 async fn main() {
-    let app = route("/", get(root));
+    let app = route("/ping", post(ping));
     let addr = SocketAddr::from(([127, 0, 0, 1], 3000));
     hyper::Server::bind(&addr)
         .serve(app.into_make_service())
@@ -11,6 +16,18 @@ async fn main() {
         .unwrap();
 }
 
-async fn root() -> &'static str {
-    "Hello, World!"
+#[derive(Deserialize)]
+struct Ping {
+    count: i64,
+}
+
+#[derive(Serialize)]
+struct Pong {
+    count: i64,
+}
+
+async fn ping(extract::Json(ping): extract::Json<Ping>) -> response::Json<Pong> {
+    response::Json(Pong{
+        count: ping.count + 1,
+    })
 }
